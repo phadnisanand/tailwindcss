4d1b89a187fcbef6053d5d8ea789f13997611707
Your Name <you@example.com>
2026-03-04 02:13:41 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,html}"
   ],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+			newfont: '#F54927',
+			secondary: {
+				100: "#E2E2D5",
+				200: "#888883"
+			}
+		}
+	},
   },
   plugins: [],
 }
